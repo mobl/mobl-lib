5029b9da856f3304048dfb8b376cdeb49eb11e41
Zef Hemel <zef@zef.me>
2011-06-15 16:31:06 +0200

    Work on supporting data migrations.

--- js/mobl.boot.js
+++ js/mobl.boot.js
@@ -25,6 +25,19 @@ mobl.load = function(url) {
     mobl.loadedFiles[url] = true;
 };
 
+mobl.initDb = function(callback) {
+  if(mobl.migration) {
+    mobl.migration.performMigration(callback)
+  } else {
+    persistence.schemaSync(function(tx) {
+      if(persistence.loadFromLocalStorage) {
+        persistence.loadFromLocalStorage();
+      }
+      callback();
+    });
+  }
+};
+
 mobl.loadingSpan = function() {
     return $("<span>Loading... <img src=\"data:image/gif;base64,R0lGODlhCgAKAJEDAMzMzP9mZv8AAP///yH/C05FVFNDQVBFMi4wAwEAAAAh+QQFAAADACwAAAAACgAKAAACF5wncgaAGgJzJ647cWua4sOBFEd62VEAACH5BAUAAAMALAEAAAAIAAMAAAIKnBM2IoMDAFMQFAAh+QQFAAADACwAAAAABgAGAAACDJwHMBGofKIRItJYAAAh+QQFAAADACwAAAEAAwAIAAACChxgOBPBvpYQYxYAIfkEBQAAAwAsAAAEAAYABgAAAgoEhmPJHOGgEGwWACH5BAUAAAMALAEABwAIAAMAAAIKBIYjYhOhRHqpAAAh+QQFAAADACwEAAQABgAGAAACDJwncqi7EQYAA0p6CgAh+QQJAAADACwHAAEAAwAIAAACCpRmoxoxvQAYchQAOw==\" width=\"10\" height=\"10\"></span>");
 };
